43425e397c9446c6fdf16718222ee094149d0cd2
lliu05 <jia988727@hotmail.com>
2016-09-25 00:19:28 -0700

    codeclimate js

--- static/js/twitchlistapp.js
+++ static/js/twitchlistapp.js
@@ -5,78 +5,80 @@
         var twitchList = this,
             //target users list
             channels = ["FreeCodeCamp", "storbeck", "terakilobyte", "Habathcx",  "RobotCaleb", "thomasballinger", "noobs2ninjas", "Beohoff", "MedryBW",  "FotiGames"];
-        twitchList.channels = {};
-        twitchList.channelArray = [];
-        twitchList.allUsers = [];
-        twitchList.onlineUsers = [];
-        twitchList.offlineUsers = [];
-        for (var i = 0; i < channels.length; i++ ) {
-          //build object lists of channels
-          twitchList.channels[channels[i]] =
-          {
-            name: channels[i],
-            api_stream: 'https://api.twitch.tv/kraken/streams/' + channels[i] + '?response_type=token&client_id=ghv9j3bobv4tkouzcob2e3p9iuqm825',
-            api_channel: 'https://api.twitch.tv/kraken/channels/' + channels[i] + '?response_type=token&client_id=ghv9j3bobv4tkouzcob2e3p9iuqm825',
-            image: "",
-            online: "", 
-            status: ""
-          }
-    }  
+            twitchList.channels = {};
+            twitchList.channelArray = [];
+            twitchList.allUsers = [];
+            twitchList.onlineUsers = [];
+            twitchList.offlineUsers = [];
+            for (var i = 0; i < channels.length; i++ ) {
+              //build object lists of channels
+              twitchList.channels[channels[i]] =
+              {
+                name: channels[i],
+                stream: 'https://api.twitch.tv/kraken/streams/' + channels[i] + '?response_type=token&client_id=ghv9j3bobv4tkouzcob2e3p9iuqm825',
+                channel: 'https://api.twitch.tv/kraken/channels/' + channels[i] + '?response_type=token&client_id=ghv9j3bobv4tkouzcob2e3p9iuqm825',
+                image: "",
+                online: "", 
+                status: ""
+              }
+        }  
     
-    for (var channel in twitchList.channels) {
-      //get user logos
-      var request_img = $http.get(twitchList.channels[channel].api_channel);
-      request_img.then(function(data) {
-        if (data.data.logo) {
-          twitchList.channels[data.data.display_name].image = data.data.logo;
-        }
-        else {
-          twitchList.channels[data.data.display_name].image = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRG9bACQ_YrbUGLe1cCOqcLdNdweXMNsJk0LHKlQtZhY0PUoOBC";    
-        }
-      }); 
-      
-      //get user stream
-      var request = $http.get(twitchList.channels[channel].api_stream);
-      request.then(function(data) {  
-        var channel_test_regex = /([^/]*)$/g;
-        var channel_test = data.data._links.self.match(channel_test_regex).join("");
-        if (data.data.stream) {
-          twitchList.channels[channel_test].online = data.data.stream;
-          twitchList.allUsers.push(twitchList.channels[channel_test]);
-          twitchList.onlineUsers.push(twitchList.channels[channel_test]);
-        }
-        else {
-          twitchList.allUsers.push(twitchList.channels[channel_test]);
-          twitchList.offlineUsers.push(twitchList.channels[channel_test]);
-        }
-      });          
-    }  
-    
-    //convert object lists to array
-    for (var c in twitchList.channels) {
-      twitchList.channelArray.push(twitchList.channels[c]);
-    }
-    
-    
-    this.tab = 1;
+        function getImage (theData) {
+            if (theData.data.logo) {
+                twitchList.channels[theData.data.display_name].image = theData.data.logo;
+            } else {
+                twitchList.channels[theData.data.display_name].image = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRG9bACQ_YrbUGLe1cCOqcLdNdweXMNsJk0LHKlQtZhY0PUoOBC";    
+            }
+        }   
 
-    this.setTab = function(newValue){
-      this.tab = newValue;
-      switch(newValue) {
-        case 1:
-          twitchList.channelArray = twitchList.allUsers;
-          break;
-        case 2:
-          twitchList.channelArray = twitchList.onlineUsers;
-          break;
-        case 3:
-          twitchList.channelArray = twitchList.offlineUsers;
-          break;
-      }
-    };
+        function getStream (theData) {
+            var channelTestRegex = /([^/]*)$/g;
+            var channelTest = theData.data._links.self.match(channelTestRegex).join("");
+            if (theData.data.stream) {
+                twitchList.channels[channelTest].online = theData.data.stream;
+                twitchList.allUsers.push(twitchList.channels[channelTest]);
+                twitchList.onlineUsers.push(twitchList.channels[channelTest]);
+            } else {
+                twitchList.allUsers.push(twitchList.channels[channelTest]);
+                twitchList.offlineUsers.push(twitchList.channels[channelTest]);   
+            } 
+        }
 
-    this.tabIsSet = function(tabName){
-      return this.tab === tabName;
-    };
-  }]); 
+        for (var channel in twitchList.channels) {
+            //get user logos
+            var requestImg = $http.get(twitchList.channels[channel].channel);
+            requestImg.then(getImage); 
+          
+            //get user stream
+            var request = $http.get(twitchList.channels[channel].stream);       
+            request.then(getStream);          
+        }  
+        
+        //convert object lists to array
+        for (var c in twitchList.channels) {
+            twitchList.channelArray.push(twitchList.channels[c]);
+        }
+        
+        
+        this.tab = 1;
+
+        this.setTab = function (newValue) {
+            this.tab = newValue;
+            switch(newValue) {
+                case 1:
+                  twitchList.channelArray = twitchList.allUsers;
+                  break;
+                case 2:
+                  twitchList.channelArray = twitchList.onlineUsers;
+                  break;
+                case 3:
+                  twitchList.channelArray = twitchList.offlineUsers;
+                  break;
+            }
+        };
+
+        this.tabIsSet = function (tabName) {
+            return this.tab === tabName;
+        };
+    }]); 
 })();
